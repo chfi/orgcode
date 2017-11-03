192c50238836269f139c0c88dec440a81aa6c55e
Christian Fischer <christian@chfi.se>
2017-11-03 20:06:17 +0100

    Extract codeblocks (cleaner & works better)

--- app/Main.hs
+++ app/Main.hs
@@ -5,18 +5,10 @@ import Lib
 
 import Data.Attoparsec.Text
 import qualified Data.Text as T
-
-import Unsafe (unsafeIndex)
+import qualified Data.List as List
 
 data Language = Purescript | Javascript | Other deriving (Eq, Ord, Show)
 
-data Codeblock a = Codeblock { lang :: Language
-                             , lineStart :: Int
-                             , lineEnd :: Int
-                             , contents :: a
-                             }
-
-type RawCodeblock = [Text]
 
 parseLanguage :: Parser Language
 parseLanguage = choice
@@ -25,17 +17,36 @@ parseLanguage = choice
   , pure Other
   ]
 
+type LOC = (Int, Text)
+type Block = [LOC]
 
-getBlock :: Text -> (Maybe RawCodeblock, [RawCodeblock]) -> (Maybe RawCodeblock, [RawCodeblock])
-getBlock t (Nothing, sofar)
-  | "#+BEGIN_SRC" `T.isPrefixOf` t = (Just [], sofar)
-  | otherwise = (Nothing, sofar) -- this could be problematic; should return Maybe ([a], [[a]])
-getBlock t (Just x, sofar)
-  | "#+END_SRC" `T.isPrefixOf` t = (Nothing, sofar <> [reverse x])
-  | otherwise = (Just (t : x), sofar)
+-- a "ParsedBlock" is one that has simply gotten the org-mode stuff stripped,
+-- with the header (everything following #+BEGIN_SRC) somehow processed and sent along (the `a`)
+type ParsedBlock a = (a, [LOC])
 
-extractCodeblocks :: [Text] -> [RawCodeblock]
-extractCodeblocks ls = snd $ foldl (flip getBlock) (Nothing, []) ls
+parseBlock :: Block -> Maybe (ParsedBlock [Text])
+parseBlock b = do
+  header <- fmap T.words $ T.stripPrefix "#+BEGIN_SRC" =<< snd <$> headMay b
+  rest <- tailMay b
+  pure (header, rest)
+
+-- run `parseLanguage` on each element in `h`, use the first success
+blockLanguage :: ParsedBlock [Text] -> Maybe (ParsedBlock Language)
+blockLanguage (h, b) = undefined
+
+nextBlock :: [LOC] -> Maybe ([LOC], [LOC])
+nextBlock ls = if List.null block'
+  then Nothing
+  else Just (block', rest')
+  where begin (_, l) = "#+BEGIN_SRC" `T.isPrefixOf` l
+        end   (_, l) = "#+END_SRC" `T.isPrefixOf` l
+        stripped = dropWhile (not . begin) ls
+        (block, rest) = break end stripped
+        block' = block <> List.take 1 rest
+        rest' = drop 1 rest
+
+extractCodeblocks :: [Text] -> [Block]
+extractCodeblocks ls = List.unfoldr nextBlock $ zip [1..] ls
 
 main :: IO ()
 main = do
@@ -49,7 +60,9 @@ main = do
       print $ length raw
 
       for_ (zip raw [1..]) $ \(cb,i) -> do
-             print $ "codeblock " <> (show i :: Text)
-             for_ cb putStrLn
+             putStrLn   ("#########################" :: Text)
+             putStrLn $ "## codeblock #" <> (show i :: Text)
+             putStrLn   ("#########################\n" :: Text)
+             for_ cb (\(i, l) -> putStrLn $ show i <> "\t" <> l)
 
     _ -> print "please provide a filename to parse"
